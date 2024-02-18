634424ca16caed3d0bf7bde7f8d2b86454c37baa
goni715 <halesgoni715149@gmail.com>
2024-02-19 02:21:29 +0600

    socket-add

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
 const app = require("./app");
 const dotenv = require('dotenv')
 dotenv.config();
+const http = require('http');
+const socketIo = require('socket.io')
+const server = http.createServer(app);
+
+const io = socketIo(server,{
+    cors: {
+        origin: '*',
+        credentials: true
+    }
+})
 
 
-app.listen(5000,function(){
+
+
+
+io.on("connection", (socket)=>{
+
+    console.log("connected"+socket.id)
+
+    //confirm-request
+    socket.on("send-notification", (data)=>{
+        if(data){
+            console.log(data);
+            io.emit("receive-notification", socket.id)
+        }
+
+    })
+
+    socket.on('disconnect', () => {
+        console.log('disconnected'+socket.id)
+    })
+
+})
+
+
+
+server.listen(5000,function(){
     console.log("Server run @5000");
 });
